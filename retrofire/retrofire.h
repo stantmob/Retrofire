232e8fde5804f9ecb1a89cd8887acf1758c40914
Rachid Calazans <rachidcalazans@gmail.com>
2017-07-09 12:15:10 -0300

    Initial commit
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

---
+++ retrofire/retrofire.h
@@ -0,0 +1,19 @@
+//
+//  retrofire.h
+//  retrofire
+//
+//  Created by Rachid Calazans on 09/07/17.
+//  Copyright © 2017 Stant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for retrofire.
+FOUNDATION_EXPORT double retrofireVersionNumber;
+
+//! Project version string for retrofire.
+FOUNDATION_EXPORT const unsigned char retrofireVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <retrofire/PublicHeader.h>
+
+
