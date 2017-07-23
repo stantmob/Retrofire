0e99da9606b8950584c3b0e2f5666d6dd7442998
Rachid Calazans <rachidcalazans@gmail.com>
2017-07-23 12:29:12 -0300

    Add doc for ErrorResponse struct
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- retrofire/ErrorResponse.swift
+++ retrofire/ErrorResponse.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Stant. All rights reserved.
 //
 
+/// Responsible to know and keep all necessary attributes to use on RemoteBase failures
 public struct ErrorResponse {
     let statusCode: Int
     let url: String
