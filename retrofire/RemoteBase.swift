90193562b2b47b5dfdb03bb42fbecd93f1f0d0c6
Rachid Calazans <rachidcalazans@gmail.com>
2018-10-31 17:45:18 -0300

    Update RemoteBase
    - remove blank spaces
    - remove unused code
    
    Signed-off-by: Rachid Calazans <rachidcalazans@gmail.com>

--- retrofire/RemoteBase.swift
+++ retrofire/RemoteBase.swift
@@ -13,9 +13,9 @@ import SwiftyJSON
 /// They need to inherited from RemoteBase.
 open class RemoteBase {
     var customErrorClass: Mappable.Type?
-    
+
     public init() {}
-    
+
     /// Return the current instance of RemoteBase
     ///
     /// - parameter klass: The Custom error class to used to Parse the error on Alamofire.request
@@ -25,10 +25,10 @@ open class RemoteBase {
         customErrorClass = klass
         return self
     }
-    
+
     /// Return a Call<ResponseObject> to be used on Remote classes implementations
     ///
-    /// - parameter request: The request object to be used on Alamofire.request. 
+    /// - parameter request: The request object to be used on Alamofire.request.
     ///
     /// - returns: Return a Call<ResponseObject> with the result object based on ResponseObject or ErrorResponse (if Alamofire gets failures) inside the Call.
     public func callSingle<ResponseObject: Mappable>(request: Request) -> Call<ResponseObject> {
@@ -37,13 +37,13 @@ open class RemoteBase {
                 .request(request.pathWithQueryParameters(), method: request.method, parameters: request.bodyParameters,
                          encoding: JSONEncoding.default, headers: request.headers)
                 .responseJSON { dataResponse in
-                    
+
                     switch(dataResponse.result) {
                     case .success(let value):
                         if self.isErrorStatusCode(response: dataResponse.response!) {
                             if let errorClass = self.customErrorClass {
                                 let error: Mappable = errorClass.instanceBy(json: JSON(value))
-                                
+
                                 exec.failed(error: error)
                                 self.resetCustomErrorClass()
                             } else {
@@ -51,15 +51,15 @@ open class RemoteBase {
                             }
                             break
                         }
-                        
+
                         let responseMapped = ParseMappableObject<ResponseObject>.parse(jsonObject: value)
-                        
+
                         if let response = responseMapped {
                             exec.success(result: response)
                         } else {
                             exec.failed(error: self.buildErrorResponseFromErroMap(klass: ResponseObject.self))
                         }
-                        
+
                         break
                     case .failure(let error):
                         exec.failed(error: self.buildErrorResponseFromDataResponse(dataResponse: dataResponse, detailMessage: error.localizedDescription))
@@ -67,10 +67,10 @@ open class RemoteBase {
                     }
             }
         }
-        
+
         return Call<ResponseObject>(alamofireFunc)
     }
-    
+
     /// Return a Call<[ResponseObject]> to be used on Remote classes implementations
     ///
     /// - parameter request: The request object to be used on Alamofire.request.
@@ -82,13 +82,13 @@ open class RemoteBase {
                 .request(request.pathWithQueryParameters(), method: request.method, parameters: request.bodyParameters,
                          encoding: JSONEncoding.default, headers: request.headers)
                 .responseJSON { dataResponse in
-                    
+
                     switch(dataResponse.result) {
                     case .success(let value):
                         if self.isErrorStatusCode(response: dataResponse.response!) {
                             if let errorClass = self.customErrorClass {
                                 let error: Mappable = errorClass.instanceBy(json: JSON(value))
-                                
+
                                 exec.failed(error: error)
                                 self.resetCustomErrorClass()
                             } else {
@@ -96,41 +96,41 @@ open class RemoteBase {
                             }
                             break
                         }
-                        
+
                         let responseMapped: [ResponseObject]? = ParseMappableObject<ResponseObject>.parseList(arrayJsonObject: value)
-                        
+
                         if let response = responseMapped {
                             exec.success(result: response)
                         } else {
                             exec.failed(error: self.buildErrorResponseFromErroMap(klass: ResponseObject.self))
                         }
                         break
-                        
+
                     case .failure(let error):
                         exec.failed(error: self.buildErrorResponseFromDataResponse(dataResponse: dataResponse, detailMessage: error.localizedDescription))
                         break
                     }
             }
         }
-        
+
         return Call<[ResponseObject]>(alamofireFunc)
     }
-    
-    
+
+
     private func resetCustomErrorClass() {
         customErrorClass = nil
     }
-    
+
     private func buildErrorResponseFromErroMap(klass: Any.Type) -> ErrorResponse {
         let detailMessage = "Error when try to map the \(klass)"
         return ErrorResponse.init(statusCode: 0, url: "", detailMessage: detailMessage)
     }
-    
+
     private func buildErrorResponseFromDataResponse(dataResponse: DataResponse<Any>, detailMessage: String = "") -> ErrorResponse {
         var url           = ""
         var statusCode    = 0
         var detailMessage = detailMessage
-        
+
         if let value = dataResponse.value {
             detailMessage = (value as AnyObject).debugDescription
         }
@@ -140,29 +140,18 @@ open class RemoteBase {
         if let requestStatusCode = dataResponse.response?.statusCode {
             statusCode = requestStatusCode
         }
-        
+
         return ErrorResponse(statusCode: statusCode, url: url, detailMessage: detailMessage)
     }
-    
+
     private func isErrorStatusCode(response : HTTPURLResponse) -> Bool {
         return !isResponseStatusCodeSuccess(response: response)
     }
-    
+
     private func isResponseStatusCodeSuccess(response : HTTPURLResponse) -> Bool{
         return response.statusCode == 200
             || response.statusCode == 201
             || response.statusCode == 203
             || response.statusCode == 304
     }
-    
-//    private func isInvalidResponseStatusCode(response: HTTPURLResponse?) -> Bool{
-//        if response == nil {
-//            return false
-//        }
-//        
-//        return response!.statusCode == 500
-//            || response!.statusCode == 401
-//            || response!.statusCode == 403
-//    }
-    
 }
